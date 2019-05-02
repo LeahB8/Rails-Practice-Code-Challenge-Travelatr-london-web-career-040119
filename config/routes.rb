a5f3a571ee8f9736b5e18d7725f281df7df0076a
LeahB8 <leahboyd08@gmail.com>
2019-05-02 17:54:51 +0100

    Work in Progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bloggers
+  resources :destinations
+  resources :posts
+
+  get '/post/:id/like', to: 'posts#increase_likes', as: 'increase_likes'
+
 end
